44f5d45d6febeb7b6d2066c8693cb197f9595cbc
Alejandro Duarte <alejandro@vaadin.com>
2017-12-21 16:17:47 +0200

    Fixes navigation with iframes & lazy loading with filtering

--- src/main/java/vaadin/service/MockPersonService.java
+++ src/main/java/vaadin/service/MockPersonService.java
@@ -1,7 +1,9 @@
 package vaadin.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import vaadin.vo.ObjectSort;
 import vaadin.vo.Person;
 
 public class MockPersonService extends MockObjectService<Person> {
@@ -19,6 +21,24 @@ public class MockPersonService extends MockObjectService<Person> {
 			persons.add(new Person("Albert", 15));
 		}
 		setRows(persons);
+	}
+
+	public List<Person> find(int offset, int limit, String name, Integer age) {
+		return findAll(offset, limit)
+				.stream()
+				.filter(p -> name == null || p.getName().toLowerCase().contains(name.toLowerCase()))
+				.filter(p -> age == null || p.getAge().equals(age))
+				.collect(Collectors.toList());
+
+	}
+
+	public int count(String name, Integer age) {
+		return (int) findAll()
+				.stream()
+				.filter(p -> name == null || p.getName().toLowerCase().contains(name.toLowerCase()))
+				.filter(p -> age == null || p.getAge().equals(age))
+				.count();
+
 	}
 
 }
